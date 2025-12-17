d979a9b810eee879f882cb5b99eea0a34eb8baf9
Dominika Krullova <domi.krullova@gmail.com>
2025-12-17 16:45:54 +0100

    backend code - cleanup

--- ATTU_TestSystem_Backend/source/dto/TestRequestDTO.h
+++ ATTU_TestSystem_Backend/source/dto/TestRequestDTO.h
@@ -5,12 +5,6 @@
 
 #include "ATTUConfigDTO.h"
 #include "Packet.h"
-
-// If these exist already, include them instead of forward-declaring
-// #include "ATTUConfigDto.hpp"
-// #include "PortDto.hpp"
-
-// ENUM(TestStatus, v_int32, VALUE(isRunning, 0));
 namespace DTO
 {
 #include OATPP_CODEGEN_BEGIN(DTO)
@@ -23,10 +17,6 @@ namespace DTO
         DTO_FIELD(String, testName);
         DTO_FIELD(String, attuIp);
         DTO_FIELD(UInt32, networkPort);
-
-        // DTO_FIELD(Object<DTO::ATTUConfigDTO>, attuConfig);
-        // DTO_FIELD(Object<DTO::Port>, port);
-
         DTO_FIELD(String, status);
     };
 
@@ -34,7 +24,7 @@ namespace DTO
     {
         DTO_INIT(TestRequestDto, DTO);
         DTO_FIELD(Enum<PacketType>::AsNumber, packet_type);
-        // DTO_FIELD(Object<TestStartRequestDto>, payload);
+        // DTO_FIELD(Object<TestStartRequestDto>, payload); //possibility to use nested DTOs
         DTO_FIELD(String, payload);
     };
 
--- ATTU_TestSystem_Backend/source/service/TestService.h
+++
@@ -1,6 +0,0 @@
-
-#pragma once
-
-#include "dto/ATTUConfigDTO.h"
-
-std::string runTest(const oatpp::Object<DTO::ATTUConfigDTO> &config);
